af835abc62329c07845118a4f2b1c1189da09f91
carus-lee <leec8.2019@gmail.com>
2023-05-23 05:42:35 +0900

    스프링부트는 임베디드DB가 기본 제공됨. - 기존에 세팅한 테스트DB 정보를 주석처리해도 정상적으로 테스트 됨. - 로그의 "url=jdbc:h2:mem:블라블라"로 확인 가능

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,7 +5,6 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
